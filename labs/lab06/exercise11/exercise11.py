8bb4cff9433b50a1ceaf425a1b2a9999ea79af81
christiangeronimo <christiangeronimo243@gmail.com>
2026-02-04 12:22:37 +0800

    lab 06

--- labs/lab06/exercise11/exercise11.py
+++ labs/lab06/exercise11/exercise11.py
@@ -1,15 +1,42 @@
 def get_student_courses(enrollments, student_id):
     """Return set of courses this student has completed."""
-    pass
+    courses = set()
+    for sid, course in enrollments:
+        if sid == student_id:
+            courses.add(course)
+    return courses
+
 
 def find_missing_courses(completed_courses, required_courses):
     """Return set of required courses not yet completed."""
-    pass
+    return required_courses - completed_courses
+
 
 def build_student_report(students, enrollments, required_courses):
-    """Return sorted list of tuples (student_id, missing_count) for students with missing courses."""
-    pass
+    report = []
+
+    for student_id in students:
+        completed = get_student_courses(enrollments, student_id)
+        missing = find_missing_courses(completed, required_courses)
+        if missing:
+            report.append((student_id, len(missing)))
+
+    for i in range(len(report)):
+        for j in range(i + 1, len(report)):
+            if report[i][1] < report[j][1]:
+                report[i], report[j] = report[j], report[i]
+            elif report[i][1] == report[j][1] and report[i][0] > report[j][0]:
+                report[i], report[j] = report[j], report[i]
+
+    return report
+
+
 
 def find_incomplete_students(enrollments, required_courses):
     """Find students who haven't completed all required courses."""
-    pass
+    students = set()
+    for sid, course in enrollments:
+        students.add(sid)
+
+    return build_student_report(students, enrollments, required_courses)
+
