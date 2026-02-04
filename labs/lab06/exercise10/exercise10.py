644f24fb3e91f53f94b4db2c370c78daab22efa4
christiangeronimo <christiangeronimo243@gmail.com>
2026-02-04 12:39:56 +0800

    lab 06

--- labs/lab06/exercise10/exercise10.py
+++ labs/lab06/exercise10/exercise10.py
@@ -1,17 +1,14 @@
 def get_unique_attendees(attendance_logs):
     """Extract set of all unique attendee IDs."""
     attendees = set()
-
     for attendee_id, event_name in attendance_logs:
         attendees.add(attendee_id)
-
     return attendees
 
 
 def count_unique_events(attendance_logs, attendee_id):
     """Count how many unique events this attendee attended."""
     events = set()
-
     for att_id, event_name in attendance_logs:
         if att_id == attendee_id:
             events.add(event_name)
@@ -22,7 +19,6 @@ def count_unique_events(attendance_logs, attendee_id):
 def filter_by_threshold(attendees, attendance_logs, min_events):
     """Return sorted list of attendees who attended >= min_events."""
     qualified = []
-
     for attendee_id in attendees:
         if count_unique_events(attendance_logs, attendee_id) >= min_events:
             qualified.append(attendee_id)
--- labs/lab06/exercise11/exercise11.py
+++ labs/lab06/exercise11/exercise11.py
@@ -13,30 +13,28 @@ def find_missing_courses(completed_courses, required_courses):
 
 
 def build_student_report(students, enrollments, required_courses):
+    """Return sorted list of tuples (missing_count, student_id) for students with missing courses."""
     report = []
 
     for student_id in students:
         completed = get_student_courses(enrollments, student_id)
         missing = find_missing_courses(completed, required_courses)
         if missing:
-            report.append((student_id, len(missing)))
+            report.append((len(missing), student_id))
 
     for i in range(len(report)):
         for j in range(i + 1, len(report)):
-            if report[i][1] < report[j][1]:
+            if report[i][0] < report[j][0]:
                 report[i], report[j] = report[j], report[i]
-            elif report[i][1] == report[j][1] and report[i][0] > report[j][0]:
+            elif report[i][0] == report[j][0] and report[i][1] > report[j][1]:
                 report[i], report[j] = report[j], report[i]
 
     return report
 
 
-
 def find_incomplete_students(enrollments, required_courses):
-    """Find students who haven't completed all required courses."""
     students = set()
     for sid, course in enrollments:
         students.add(sid)
 
     return build_student_report(students, enrollments, required_courses)
-
